1982137292bcca142e96d581fea36858f866fb0e
hjiayuan <jiayuan.han.2021@scis.smu.edu.sg>
2025-03-18 23:28:50 +0800

    Update pDeck.java
    added a few things that could be missing in pDeck

--- pDeck.java
+++ pDeck.java
@@ -1,28 +1,38 @@
-class pDeck {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class pDeck {
     private ArrayList<Card> deck;
 
     public pDeck(ArrayList<Card> deck) {
         this.deck = deck;
     }
 
-    public getParadeDeck() {
+    public ArrayList<Card> getParadeDeck() {
         return deck;
     }
 
-    public getNumberOfCards() {
+    public int getNumberOfCards() {
         return deck.size();
     }
 
-    public void setDeck(ArrayList<Card> deck) {
-        this.deck = deck;
+  
+    // Added shuffle deck method
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
     }
+
+
 
     /**
      * Issues a Card object from the top of the deck.
-     * 
+     * Added isEmpty method to check if the deck is empty
      * @return Card object
      */
     public Card issueCard() {
+        if (deck.isEmpty()) {
+        return null; // or throw an exception
+    }
         return deck.remove(deck.size() - 1);
     }
 }
